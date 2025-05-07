5a2573740bd43d6bbd8816fa8c9de0021d8fb9fc
ehsan malek <EhsanMalekBS@gmail.com>
2025-05-07 11:00:25 +0330

    Added existence check before deleting user in delete_user.php

--- actions/delete_user.php
+++ actions/delete_user.php
@@ -1,15 +1,23 @@
 <?php
+require_once '../includes/auth.php';
+require_admin();
 require_once '../config/db.php';
-if(isset($_GET['delete'])){
-    $user_id = intval($_GET['delete']);
 
+$user_id = intval($_GET['user_id']);
+
+$check_sql = "SELECT * FROM users WHERE id = :user_id";
+$check_query = $pdo->prepare($check_sql);
+$check_query->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+$check_query->execute();
+
+if ($check_query->rowCount() > 0) {
     $sql = "DELETE FROM users WHERE id=:id";
 
     $delete_query = $pdo->prepare($sql);
     $delete_query->bindParam(':id', $user_id, PDO::PARAM_INT);
     $delete_query->execute();
     echo "<script>alert('User Deleted successfully!');</script>";
-    echo "<script>window.location.href='../public/index.php';</script>";
-//        header("Location: users.php");
+}else{
+    echo "<script>alert('User does not exist!');</script>";
 }
-?>
+echo "<script>window.location.href='../public/index.php';</script>";
