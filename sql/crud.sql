9a024e80c545f03dcbdb908d3cc1d13ffcd8c522
ehsan malek <EhsanMalekBS@gmail.com>
2025-05-07 10:36:59 +0330

    update database for superadmin and allowed user categories to modified

--- sql/crud.sql
+++ sql/crud.sql
@@ -1,26 +1,28 @@
-# create table users(
-#   id int auto_increment primary key ,
-#   username varchar(100) not null ,
-#   phone_number varchar(20) not null unique ,
-#     email varchar(100),
-#     address text,
-#     source varchar(100),
-#     category ENUM('A', 'B', 'C') default 'C',
-#     created_at timestamp default current_timestamp
-# );
-#
-# create table notes (
-#     id int auto_increment primary key ,
-#     user_id int not null ,
-#     title varchar(100),
-#     note text not null ,
-#     contact_date datetime default current_timestamp,
-#     foreign key (user_id) references users(id) on delete cascade
-# );
+CREATE TABLE users (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   username VARCHAR(100) NOT NULL,
+   phone_number VARCHAR(20) NOT NULL UNIQUE,
+   email VARCHAR(100),
+   address TEXT,
+   source VARCHAR(100),
+   category ENUM('A', 'B', 'C') DEFAULT 'C',
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-# create table admins(
-#     id INT auto_increment primary key ,
-#     username varchar(50) not null unique ,
-#     password varchar(255) not null ,
-#     fullname varchar(100)
-# );
+CREATE TABLE notes (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   user_id INT NOT NULL,
+   title VARCHAR(100),
+   note TEXT NOT NULL,
+   contact_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE admins (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    fullname VARCHAR(100),
+    role ENUM('superadmin', 'admin') NOT NULL DEFAULT 'admin',
+    allowed_categories TEXT
+);
