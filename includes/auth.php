7f2628e9e75922e5202f669a7a495f7aae6236af
ehsan malek <EhsanMalekBS@gmail.com>
2025-05-07 10:57:05 +0330

    Added require_superadmin() function to auth.php for Superadmin-only access control

--- includes/auth.php
+++ includes/auth.php
@@ -2,6 +2,12 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+function require_superadmin() {
+    if(!isset($_SESSION['admin_role']) || $_SESSION['admin_role'] !== 'superadmin'){
+        header('Location: ../public/index.php');
+        exit;
+    }
+}
 
 function require_admin(){
     if(!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'admin'){
